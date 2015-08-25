9b03b283e3f279356e714013ba93417a4d8cd5c3
Curt Zimmerman <czimmerman9@gmail.com>
2015-08-25 13:35:20 -0400

    add links to project selector

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ $( document ).ready	(function () {
 	var $body = $('html, body');
 	var $navLinks = $('span.links a');
 
-
+	//Fade in logo in header when greeting is scrolled past
 	var fadeLogo = function() {
 		if ($( window ).scrollTop() > ($heading.offset().top + $heading.outerHeight() - $header.outerHeight())) {
 			$hiddenLogo.css('visibility', 'visible');
@@ -17,14 +17,20 @@ $( document ).ready	(function () {
 		}
 	}
 
+	//Jump to section when link is clicked
 	var jumpToLocation = function(location) {
 		$location = $('div.' + location);
 		$body.animate({scrollTop: $location.offset().top}, 800);
 	}
 
+
+
+	//initialize
 	fadeLogo();
 	$( window ).scroll(fadeLogo);
 
+
+	//todo: fix flashing on some clicks
 	$navLinks.click(function() {
 		$('a.selected').removeClass('selected');
 		$(this).addClass('selected');
