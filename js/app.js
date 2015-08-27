ac4edaf2b4da9ccd519b72f6a0e18547db4e87e2
Curt Zimmerman <czimmerman9@gmail.com>
2015-08-27 12:10:07 -0400

    refactor app.js to modular javascript

--- js/app.js
+++ js/app.js
@@ -1,41 +1,44 @@
-$( document ).ready	(function () {
+var animationsModule = (function() {
 
+	// Set jQuery animations rate
+	jQuery.fx.interval = 6; //default 13ms
+
+	// Cache DOM
+	var $html = $('html');
+	var $header = $('.header');
 	var $heading = $('.heading');
-	var $header = $('header');
-	var $hiddenLogo = $('a.hidden');
-	var $navBar = $('');
-	var $body = $('html');
-	var $navLinks = $('span.links a');
+	var $logo = $header.find("#logo");
+	var $links = $('.links');
 
-	//Fade in logo in header when greeting is scrolled past
-	var fadeLogo = function() {
-		if ($( window ).scrollTop() > ($heading.offset().top + $heading.outerHeight() - $header.outerHeight())) {
-			$hiddenLogo.css('visibility', 'visible');
-			$hiddenLogo.fadeIn("slow");
+	//Bind Events
+	$(window).on('scroll', fadeInLogo);
+	$('.links').find('a').on('click', jumpToSection.bind(this));
+
+	// if the top of the window (minus header height) is lower than the bottom edge of the greeting, show the logo, else hide it 
+	function fadeInLogo() {		
+		if ( ($(window).scrollTop() - $header.outerHeight()) > ($heading.offset().top + $heading.find("h1").outerHeight())) {
+			$logo.css('visibility', 'visible');
+			$logo.fadeIn('slow');
 		} else {
-			$hiddenLogo.fadeOut("slow");
+			$logo.fadeOut('slow');
 		}
 	}
 
-	//Jump to section when link is clicked
-	var jumpToLocation = function(location) {
-		$location = $('div.' + location);
-		$body.animate({scrollTop: $location.offset().top}, 800);
+	// Jump to top of div with matching class name if link is clicked
+	function jumpToSection(location) {
+		$location = $(location.target);
+		$location.siblings('.selected').removeClass('selected');
+		$location.addClass('selected');
+		$scrollPosition = $('div.' + $location.text().toLowerCase());
+		$html.animate({scrollTop: $scrollPosition.offset().top}, 600);
+		return false;
 	}
 
+	return {
+		fadeInLogo: fadeInLogo,
+		jumpToSection: jumpToSection
+	};
 
-
-	//initialize
-	fadeLogo();
-	$( window ).scroll(fadeLogo);
-
-
-	//todo: fix flashing on some clicks
-	$navLinks.click(function() {
-		$('a.selected').removeClass('selected');
-		$(this).addClass('selected');
-		console.log($(this).text().toLowerCase());
-		jumpToLocation($(this).text().toLowerCase());
-		return false;
-	});
 });
+
+$(document).ready(animationsModule);
