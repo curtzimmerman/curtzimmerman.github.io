5acff38778f35ea913a8e3df0cf650d5ac2bbd91
Curt Zimmerman <czimmerman9@gmail.com>
2015-08-27 15:17:30 -0400

    change link highlighting based on scroll position

--- js/app.js
+++ js/app.js
@@ -9,9 +9,13 @@ var animationsModule = (function() {
 	var $heading = $('.heading');
 	var $logo = $header.find("#logo");
 	var $links = $('.links');
+	var $home = $('div.home');
+	var $projects = $('div.projects');
+	var $contact = $('div.contact');
 
 	//Bind Events
 	$(window).on('scroll', fadeInLogo);
+	$(window).on('scroll', highlightLinkOnScroll);
 	$('.links').find('a').on('click', jumpToSection.bind(this));
 
 	// if the top of the window (minus header height) is lower than the bottom edge of the greeting, show the logo, else hide it 
@@ -26,14 +30,32 @@ var animationsModule = (function() {
 
 	// Jump to top of div with matching class name if link is clicked
 	function jumpToSection(location) {
-		console.log(location);
 		$location = $(location.target);
-		$location.siblings('.selected').removeClass('selected');
-		$location.addClass('selected');
 		$scrollPosition = $('div.' + $location.text().toLowerCase());
 		topMargin = parseInt($scrollPosition.css("margin-top"));
 		$html.animate({scrollTop: $scrollPosition.offset().top - topMargin - $header.outerHeight()}, 600);
-		console.log($scrollPosition);
+		return false;
+	}
+
+	function removeAllSelected(element) {
+		element.children('.selected').removeClass('selected');
+	}
+
+	function highlightLinkOnScroll() {
+		var windowCenter = ($(window).scrollTop() + ($(window).height() / 2));
+		var projectsTop = $projects.offset().top;
+		var contactTop = $contact.offset().top;
+
+		removeAllSelected($('.links'));
+		
+		// highlight link based on middle of window
+		if (windowCenter < projectsTop) {
+			$links.find(".home").addClass("selected");
+		} else if (windowCenter > projectsTop && windowCenter < contactTop) {
+			$links.find(".projects").addClass("selected");
+		} else {
+			$links.find(".contact").addClass("selected");
+		}
 		return false;
 	}
 
